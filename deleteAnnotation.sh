87c9d0aa3c8da775c4282996854d5407c0adcf69
ARGH0 <82467127+ARGO492@users.noreply.github.com>
2024-06-24 10:29:37 +0200

    refactored the deleteAnnotation and introduced the possibility to delete multiple records

--- deleteAnnotation.sh
+++ deleteAnnotation.sh
@@ -1,49 +1,61 @@
 #!/bin/bash
-
 set -e
 
-grafana_server=""
-
-BUILD_NUMBER="1"
-application_name="application_name"
-
-token=""
-buildtagsearch="build:${BUILD_NUMBER}"
-servicetagsearch="service:${application_name}"
-
-echo ""
-echo "||#####################||"
-echo "Searching for annotations with tags: ${buildtagsearch} and ${servicetagsearch}"
-echo "------"
-
-get_result=$(curl -X GET "${grafana_server}/api/annotations?tags=${buildtagsearch}&tags=${servicetagsearch}" \
-    -H "Accept: application/json" \
-    -H "Content-Type: application/json" \
-    -H "Authorization: Bearer ${token}")
-
-if [ -z "${get_result}" ]; then
+delete_annotation() {
+    local annotation_id="$1"
+    local delete_result=$(curl -X DELETE "${grafana_server}/api/annotations/${annotation_id}" \
+        -H "Accept: application/json" \
+        -H "Content-Type: application/json" \
+        -H "Authorization: Bearer ${grafana_token}")
     echo ""
-    echo "||!!!!!!!!!!!!!!!!!!!!!||"
-    echo "No annotations found"
-    echo "------"
-    exit 1
-fi
+    echo "||#####################||"
+    if [ -z "${delete_result}" ]; then
+        echo "Failed to delete annotation: ${annotation_id}"
+    else
+        echo "Annotation deleted: ${annotation_id}"
+    fi
+}
 
-annotation_id="$(jq -r '.[] | .id' <<<"${get_result}")"
+main() {
+    local grafana_server="$1"
+    local grafana_token="$2"
+    local environment="$5"
+    local build_number="$6"
+    local component="$7"
 
-if [ "$(wc -w <<< "${annotation_id}")" -gt 1 ]; then
+    buildtagsearch="build:${BUILD_NUMBER}"
+    servicetagsearch="service:${component}"
+
     echo ""
-    echo "||!!!!!!!!!!!!!!!!!!!!!||"
-    echo "Multiple annotations found"
+    echo "||#####################||"
+    echo "Searching for annotations with tags: ${buildtagsearch} and ${servicetagsearch}"
     echo "------"
-    exit 1
-fi
 
-delete_result=$(curl -X DELETE "${grafana_server}/api/annotations/${annotation_id}" \
-    -H "Accept: application/json" \
-    -H "Content-Type: application/json" \
-    -H "Authorization: Bearer ${token}")
+    get_result=$(curl -X GET "${grafana_server}/api/annotations?tags=${buildtagsearch}&tags=${servicetagsearch}" \
+        -H "Accept: application/json" \
+        -H "Content-Type: application/json" \
+        -H "Authorization: Bearer ${grafana_token}")
 
-echo ""
-echo "||#####################||"
-echo "Annotation deleted: ${annotation_id}"
+    if [ -z "${get_result}" ]; then
+        echo ""
+        echo "||!!!!!!!!!!!!!!!!!!!!!||"
+        echo "No annotations found"
+        echo "------"
+        exit 1
+    fi
+
+    annotation_ids=($(jq -r '.[] | .id' <<<"${get_result}"))
+    if [ ${#annotation_ids[@]} -eq 0 ]; then
+        echo ""
+        echo "||!!!!!!!!!!!!!!!!!!!!!||"
+        echo "No annotations found"
+        echo "------"
+        exit 1
+    fi
+    for annotation_id in "${annotation_ids[@]}"; do
+        delete_annotation "${annotation_id}"
+    done
+}
+
+# Call the main function
+main "$@"
