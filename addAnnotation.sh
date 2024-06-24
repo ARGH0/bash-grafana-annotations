1d83aa42d06b30580451e54de76bb2bbc0721d22
ARGH0 <82467127+ARGO492@users.noreply.github.com>
2024-06-24 11:06:54 +0200

    refactor: refactor annotation script to improve modularity and readability

--- addAnnotation.sh
+++ addAnnotation.sh
@@ -1,55 +1,141 @@
 #!/bin/bash
 
+# Script to add an annotation to Grafana
+
 set -e
 
-grafana_server=""
+build_tags() {
+    # Build tags based on input parameters
+    # Parameters:
+    #   - kind: The kind of annotation
+    #   - type: The type of annotation
+    #   - environment: The environment for the annotation
+    #   - build_number: The build number for the annotation
+    #   - component: The component for the annotation (optional)
+    #   - image_tag: The image tag for the annotation (optional)
+    #   - collection: The collection for the annotation (optional)
+    # Returns:
+    #   - tags: A comma-separated string of tags
+    local kind="$1"
+    local type="$2"
+    local environment="$3"
+    local build_number="$4"
+    local component="$5"
+    local image_tag="$6"
+    local collection="$7"
 
-environment="test"
-BUILD_NUMBER="1"
-application_name="application_name"
+    local tags=("kind:${kind}" "type:${type}" "env:${environment}" "build:${build_number}")
 
-token=""
-tags=("kind:deployment" "env:${environment}" "service:${application_name}" "build:${BUILD_NUMBER}")
+    if [[ -n "$component" ]]; then
+        tags+=("component:${component}")
+    fi
 
-current_time_seconds=$((1000 * $(date +%s)))
-#echo $(date -d@"$(($current_time_seconds / 1000))")
+    if [[ -n "$image_tag" ]]; then
+        tags+=("image:${image_tag}")
+    fi
 
-# Convert the tags array into a string with specific formatting
-tags_string=$(printf '"%s",' "${tags[@]}" | sed 's/,$//')
+    if [[ -n "$collection" ]]; then
+        tags+=("collection:${collection}")
+    fi
 
-# create an annotation request body
-json_string="$(
-    jq -n --arg time "$current_time_seconds" \
-        --arg timeEnd "$current_time_seconds" \
-        --argjson tags "[$tags_string]" \
-        --arg text "Running period of deployment for: ${application_name}" \
-        '{
-            "time": ($time | tonumber),
-            "timeEnd": ( $timeEnd | tonumber),
-            "tags": $tags,
-            "text": $text
-        }'
-)"
+    echo $(printf '"%s",' "${tags[@]}" | sed 's/,$//')
+}
 
-#echo "${grafana_server}/api/annotations"
-#echo "${json_string}"
+build_text() {
+    # Build text based on input parameters
+    # Parameters:
+    #   - type: The type of annotation
+    #   - component: The component for the annotation
+    #   - environment: The environment for the annotation
+    #   - build_number: The build number for the annotation
+    #   - collection: The collection for the annotation
+    # Returns:
+    #   - text: The built text for the annotation
+    local type="$1"
+    local component="$2"
+    local environment="$3"
+    local build_number="$4"
+    local collection="$5"
 
-echo ""
-echo "||#####################||"
-echo "Creating an annotation with: "
-echo "tags: ${tags_string}"  
-echo "time: $(date -d@"$((current_time_seconds / 1000))")"
-echo "------"
-# Send an annotation to Grafana
-post_result=$(curl -X POST "${grafana_server}/api/annotations" \
-    -H "Accept: application/json" \
-    -H "Content-Type: application/json" \
-    -H "Authorization: Bearer ${token}" \
-    -d "${json_string}")
-echo "${post_result}"
+    local text=""
 
-annotation_id="$(jq -r '.id' <<<"${post_result}")"
+    if [[ "$kind" == "deployment_pipeline" ]]; then
+        text="Deployment for ${type} pipeline ${build_number} to ${environment}"
+    fi
 
-echo ""
-echo "||#####################||"
-echo "Annotation created: ${annotation_id}"
+    if [[ -z "$text" ]]; then
+        if [[ "$type" == "service" ]]; then
+            text="Deploying ${component} to ${environment}"
+        elif [[ "$type" == "infrastructure" ]]; then
+            text="Deploying ${component} to ${environment}"
+        elif [[ "$type" == "monitoring" ]]; then
+            text="Deploying ${component} to ${environment}"
+        elif [[ "$type" == "infrajobs" ]]; then
+            text="Running job ${collection} on ${environment}"
+        else
+            text="No specification for type: ${type}"
+        fi
+    fi
+
+    echo "$text"
+}
+
+main() {
+    # Main function to add an annotation to Grafana
+    # Parameters:
+    #   - grafana_server: The URL of the Grafana server
+    #   - grafana_token: The authentication token for Grafana
+    #   - kind: The kind of annotation
+    #   - type: The type of annotation
+    #   - environment: The environment for the annotation
+    #   - build_number: The build number for the annotation
+    #   - component: The component for the annotation (optional)
+    #   - image_tag: The image tag for the annotation (optional)
+    #   - collection: The collection for the annotation (optional)
+    #   - pipeline_type: The type of pipeline (optional)
+    local grafana_server="$1"
+    local grafana_token="$2"
+    local kind="$3"
+    local type="$4"
+    local environment="$5"
+    local build_number="$6"
+    local component="$7"
+    local image_tag="$8"
+    local collection="$9"
+    local pipeline_type="${10}"
+
+    local tags_string=$(build_tags "$kind" "$type" "$environment" "$build_number" "$component" "$image_tag" "$collection" "$pipeline_type")
+    local text=$(build_text "$type" "$component" "$environment" "$build_number" "$collection")
+
+    local current_time_seconds=$((1000 * $(date +%s)))
+
+    # create an annotation request body
+    local json_string="$(
+        jq -n --arg time "$current_time_seconds" \
+            --arg timeEnd "$current_time_seconds" \
+            --argjson tags "[$tags_string]" \
+            --arg text "$text" \
+            '{
+                "time": ($time | tonumber),
+                "timeEnd": ( $timeEnd | tonumber),
+                "tags": $tags,
+                "text": $text
+            }'
+    )"
+
+    # Send an annotation to Grafana
+    local post_result=$(curl -X POST "${grafana_server}/api/annotations" \
+        -H "Accept: application/json" \
+        -H "Content-Type: application/json" \
+        -H "Authorization: Bearer ${grafana_token}" \
+        -d "${json_string}")
+
+    local annotation_id="$(jq -r '.id' <<<"${post_result}")"
+
+    echo ""
+    echo "||#####################||"
+    echo "Annotation created: ${annotation_id}"
+}
+
+# Call main function with all arguments
+main "$@"
